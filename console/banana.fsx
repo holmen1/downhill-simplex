8215bf0fcd6a99792a96c565c30b95aa3b52ad8e
holmen1 <holmen1@gmail.com>
2020-07-25 21:35:55 +0200

    Add Record and sorting

--- console/banana.fsx
+++ console/banana.fsx
@@ -6,6 +6,20 @@ let bananaFcn ((a,b): float*float) ((x,y): float*float) =
 
 let objFcn = bananaFcn (1.0, 100.0)
 
+// Record to be sorted
+type valuePair = {fx:float; x:(float*float)}
+let v0 = {fx=0.23; x=(0.0, 9.99)}
+let v1 = {fx=1.23; x=(1.0, 9.99)}
+let v2 = {fx=2.31; x=(2.0, 9.99)}
+let v3 = {fx=3.12; x=(3.0, 9.99)}
+let valueList = [v2; v3; v0; v1]
+
+valueList |> List.iter (fun s -> printf "fx: %f\n" s.fx)
+printf "\n"
+valueList |> List.sortBy (fun s -> s.fx)
+|> List.iter (fun s -> printf "fx: %f\n" s.fx)
+
+
 let fit objective initGuess =
     // to be implemented ...
     initGuess
