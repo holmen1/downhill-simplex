523a27f91d35ac84a122f3b28a71fab2c6a3187e
holmen1 <holmen1@gmail.com>
2020-07-26 08:10:32 +0200

    New order

--- console/banana.fsx
+++ console/banana.fsx
@@ -6,26 +6,40 @@ let bananaFcn ((a,b): float*float) ((x,y): float*float) =
 
 let objFcn = bananaFcn (1.0, 100.0)
 
-// Record to be sorted
-type valuePair = {fx:float; x:(float*float)}
-let v0 = {fx=0.23; x=(0.0, 9.99)}
-let v1 = {fx=1.23; x=(1.0, 9.99)}
-let v2 = {fx=2.31; x=(2.0, 9.99)}
-let v3 = {fx=3.12; x=(3.0, 9.99)}
-let valueList = [v2; v3; v0; v1]
-
-valueList |> List.iter (fun s -> printf "fx: %f\n" s.fx)
-printf "\n"
-valueList |> List.sortBy (fun s -> s.fx)
-|> List.iter (fun s -> printf "fx: %f\n" s.fx)
+// Returns list [(f(x1), x1); (f(x2), x2); ...; (f(xn+1), xn+1)]
+// s. t. f(x1) <= f(x2) <= ... <= f(xn+1)
+let orderVertices valueVertexPairs =
+    valueVertexPairs
+    |> List.sortBy fst
 
 
 let fit objective initGuess =
     // to be implemented ...
     initGuess
 
-// Test given-when-then
+// TESTS ---- given-when-then ----
+
+// Sort
+let t0 = 0.23, (0.0, 9.99)
+let t1 = 1.23, (0.0, 9.99)
+let t2 = 2.23, (0.0, 9.99)
+let t3 = 3.23, (0.0, 9.99)
+let valueList = [t2; t3; t0; t1]
+let sortedList = orderVertices valueList
+printfn "Passed the sorting-test : %b" (sortedList = [t0; t1; t2; t3])
+
+// Main
 let init = (-0.5, 3.0)
 let result = fit objFcn init
 let success = result = (1.0, 1.0)
 printfn "Passed the banana-test : %b" success
+
+
+
+
+
+
+
+
+
+
