6d783c00179ddfc48e0143c918a76b6eb7d7445c
holmen1 <holmen1@gmail.com>
2023-04-16 09:16:41 +0200

    Upgrade to net6.0

--- DownhillSimplex/DownhillSimplex.fs
+++ DownhillSimplex/DownhillSimplex.fs
@@ -1,39 +1,37 @@
 ﻿namespace DownhillSimplex.FSharp
 
-type Vertex = Vertex of float list with
-    static member (+) (Vertex u, Vertex v) = List.map2 (+) u v |> Vertex
-    static member (-) (Vertex u, Vertex v) = List.map2 (-) u v |> Vertex
-    static member ( * ) (a: float, Vertex v) = List.map (fun x -> a * x) v |> Vertex
-    static member ( / ) (Vertex v, a: float) = List.map (fun x -> x / a) v |> Vertex
+type Vertex =
+    | Vertex of float list
+
+    static member (+)(Vertex u, Vertex v) = List.map2 (+) u v |> Vertex
+    static member (-)(Vertex u, Vertex v) = List.map2 (-) u v |> Vertex
+    static member (*)(a: float, Vertex v) = List.map (fun x -> a * x) v |> Vertex
+    static member (/)(Vertex v, a: float) = List.map (fun x -> x / a) v |> Vertex
 
 // Nelder-Mead
 [<AbstractClass>]
 type DownhillSimplex(init: Vertex) =
 
-    let alpha = 1.0       // reflection coefficient
-    let gamma = 2.0       // expansion -""-
-    let rho = 0.5         // contraction -""-
-    let bumpfactor = 1.2  // initial vertex perturbation
+    let alpha = 1.0 // reflection coefficient
+    let gamma = 2.0 // expansion -""-
+    let rho = 0.5 // contraction -""-
+    let bumpfactor = 1.2 // initial vertex perturbation
 
     let toList (Vertex v) = v
     let Length (Vertex v) = v.Length
 
     let log = System.Math.Log
     let exp = System.Math.Exp
-      
+
     // bumps v[index] -> f(v[index])
-    let bump (index: int) (f: float->float) (Vertex v) =
-        List.mapi (fun i x -> if i = index then f x else x) v
-        |> Vertex
+    let bump (index: int) (f: float -> float) (Vertex v) =
+        List.mapi (fun i x -> if i = index then f x else x) v |> Vertex
 
-    let reflection (xc: Vertex) (xh: Vertex) =
-        xc + alpha * (xc - xh)
+    let reflection (xc: Vertex) (xh: Vertex) = xc + alpha * (xc - xh)
 
-    let expansion (x': Vertex) (xc: Vertex) =
-        x' + gamma * (x' - xc)
+    let expansion (x': Vertex) (xc: Vertex) = x' + gamma * (x' - xc)
 
-    let contraction (xc: Vertex) (xh: Vertex) =
-        xc - rho * (xc - xh)
+    let contraction (xc: Vertex) (xh: Vertex) = xc - rho * (xc - xh)
 
     let shrink (l: int) (simplex: Vertex list) =
         let xl = simplex.Item(l)
@@ -41,31 +39,30 @@ type DownhillSimplex(init: Vertex) =
 
     let rec remove (i: int) simplex =
         match i, simplex with
-        | 0, _::xs -> xs
-        | i, x::xs -> x::remove (i - 1) xs
+        | 0, _ :: xs -> xs
+        | i, x :: xs -> x :: remove (i - 1) xs
         | _, [] -> failwith "index out of range"
 
     let makeSimplex (v: Vertex) =
         let n = Length v
-        v::List.init n (fun i -> bump i (fun x -> 1.2 * x) v)
+        v :: List.init n (fun i -> bump i (fun x -> 1.2 * x) v)
 
     // argMax/argMin
     // returns (i, f(vertex_i))
     let argMax (f: Vertex -> float) (simplex: Vertex list) =
-        List.mapi (fun i v -> (i, f v)) simplex
-        |> List.maxBy snd
+        List.mapi (fun i v -> (i, f v)) simplex |> List.maxBy snd
 
     let argMin (f: Vertex -> float) (simplex: Vertex list) =
-        List.mapi (fun i v -> (i, f v)) simplex
-        |> List.minBy snd
+        List.mapi (fun i v -> (i, f v)) simplex |> List.minBy snd
 
     // arithmetic mean vertex of simplex
     let centroid (simplex: Vertex list) =
         let rec sumSimplex (simplex: Vertex list) =
             match simplex with
-                | head :: tail  when tail.IsEmpty -> head
-                | head :: tail -> head + (sumSimplex tail)
-                | [] -> failwith "warning FS0025"
+            | head :: tail when tail.IsEmpty -> head
+            | head :: tail -> head + (sumSimplex tail)
+            | [] -> failwith "warning FS0025"
+
         (sumSimplex simplex) / (float simplex.Length)
 
 
@@ -82,25 +79,32 @@ type DownhillSimplex(init: Vertex) =
         let x' = reflection xc xh
         let f' = objFn x' // value on reflected point
         //if (fhigh - flow) < tol then (simplex, true) // converged
-        if flow < tol then (simplex, true) // converged
+        if flow < tol then
+            (simplex, true) // converged
         elif f' < flow then
             let x'' = expansion x' xc
             let f'' = objFn x''
-            if f'' < flow then (x''::simplex', false)
-            else (x'::simplex', false)
+
+            if f'' < flow then
+                (x'' :: simplex', false)
+            else
+                (x' :: simplex', false)
         elif f' > fh' then
-            if f' <= fhigh then (x'::simplex', false)
+            if f' <= fhigh then
+                (x' :: simplex', false)
             else
                 let x'' = contraction xc xh
                 let f'' = objFn x''
-                if f'' > fhigh then (shrink l simplex, false)
-                else (x''::simplex', false)
-        else (x'::simplex', false)
+
+                if f'' > fhigh then
+                    (shrink l simplex, false)
+                else
+                    (x'' :: simplex', false)
+        else
+            (x' :: simplex', false)
 
     abstract member objective: Vertex -> float
-    member this.obj (vs: float list) =
-        Vertex vs
-        |> this.objective
+    member this.obj(vs: float list) = Vertex vs |> this.objective
 
     // convergence criterion on cost function
     abstract member tol: float
@@ -108,16 +112,18 @@ type DownhillSimplex(init: Vertex) =
 
     member this.initGuess = init
 
-    member this.fit (v: Vertex) =
+    member this.fit(v: Vertex) =
         let maxiter = 5000
         let mutable iter = 0
         let mutable simplex = makeSimplex v
         let mutable converged = false
+
         while (not converged && iter < maxiter) do
             let s, c = downhill this.objective simplex this.tol
             simplex <- s
             converged <- c
             iter <- iter + 1
+
         let l, flow = argMin this.objective simplex
         let res = simplex.Item(l)
         toList res
--- DownhillSimplex/DownhillSimplex.fsi
+++ DownhillSimplex/DownhillSimplex.fsi
@@ -3,44 +3,58 @@
 
 
 namespace DownhillSimplex.FSharp
-  type Vertex =
-    | Vertex of float list
-    with
-      static member ( + ) : Vertex * Vertex -> Vertex
-      static member ( / ) : Vertex * a:float -> Vertex
-      static member ( * ) : a:float * Vertex -> Vertex
-      static member ( - ) : Vertex * Vertex -> Vertex
-    end
-  [<AbstractClassAttribute ()>]
-  type DownhillSimplex =
-    class
-      new : init:Vertex -> DownhillSimplex
-      abstract member tol : float
-      abstract member objective : Vertex -> float
-      member fit : v:Vertex -> float list
-      member initGuess : Vertex
-      override tol : float
-      member obj : vs:float list -> float
-    end
+    
+    type Vertex =
+        | Vertex of float list
+        
+        static member ( * ) : a: float * Vertex -> Vertex
+        
+        static member (+) : Vertex * Vertex -> Vertex
+        
+        static member (-) : Vertex * Vertex -> Vertex
+        
+        static member (/) : Vertex * a: float -> Vertex
+    
+    [<AbstractClass>]
+    type DownhillSimplex =
+        
+        new: init: Vertex -> DownhillSimplex
+        
+        member fit: v: Vertex -> float list
+        
+        member obj: vs: float list -> float
+        
+        abstract objective: Vertex -> float
+        
+        member initGuess: Vertex
+        
+        abstract tol: float
+        
+        override tol: float
 
 namespace DownhillSimplex.FSharp
-  type OptimalDiscountFactors =
-    class
-      inherit DownhillSimplex
-      new : p1:float * p2:float * p3:float * p4:float * p5:float * p6:float *
-            p7:float * p8:float * p9:float * p10:float * p12:float * p15:float *
-            p20:float -> OptimalDiscountFactors
-      member discountFactor : dur:int -> float
-      member fiRate : dur:int -> float
-      override tol : float
-      override objective : Vertex -> float
-    end
+    
+    type OptimalDiscountFactors =
+        inherit DownhillSimplex
+        
+        new: p1: float * p2: float * p3: float * p4: float * p5: float *
+             p6: float * p7: float * p8: float * p9: float * p10: float *
+             p12: float * p15: float * p20: float -> OptimalDiscountFactors
+        
+        member discountFactor: dur: int -> float
+        
+        member fiRate: dur: int -> float
+        
+        override objective: Vertex -> float
+        
+        override tol: float
 
 namespace DownhillSimplex.FSharp
-  type MinimizeBanana =
-    class
-      inherit DownhillSimplex
-      new : x:float * y:float -> MinimizeBanana
-      override objective : Vertex -> float
-    end
+    
+    type MinimizeBanana =
+        inherit DownhillSimplex
+        
+        new: x: float * y: float -> MinimizeBanana
+        
+        override objective: Vertex -> float
 
